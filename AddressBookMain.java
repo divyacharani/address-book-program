b725dcb2cd2d091ead1305e582ab18629b722ddc
unknown <divyacharaninabha@gmail.com>
2020-09-28 09:54:33 +0530

    Added: Welcome message added

---
+++ AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressBookProgram;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Welcome message
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
