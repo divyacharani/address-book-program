d17ad34057addcafc982eb26dcc83f4feb4f9872
unknown <divyacharaninabha@gmail.com>
2020-09-28 10:30:06 +0530

    Added: Contact added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,12 +1,50 @@
 package com.bridgelabz.addressBookProgram;
 
+import java.util.Scanner;
+
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		//Welcome message
 		System.out.println("Welcome to Address Book Program");
+
+		Contacts person = new Contacts();
+		int option= 1;
+		System.out.println("Welcome to Address Book Program");
+		Scanner sc = new Scanner(System.in);
+		
+		//To add a contact
+		while(option>0 && option<2) {
+			System.out.println("Enter your option\n1. Add Contact\n2. Exit");
+			option = sc.nextInt();
+			if(option==1) {
+				System.out.println("Enter the details to add contact");
+				System.out.println("Enter First Name");
+				sc.nextLine();
+				String firstName = sc.nextLine();
+				System.out.println("Enter Last Name");
+				String lastName = sc.nextLine();
+				System.out.println("Enter Address");
+				String  address= sc.nextLine();
+				System.out.println("Enter City");
+				String city = sc.nextLine();
+				System.out.println("Enter State");
+				String  state= sc.nextLine();
+				System.out.println("Enter Zip");
+				int  zip= sc.nextInt();
+				System.out.println("Enter Phone Number");
+				long phoneNumber = sc.nextLong();
+				System.out.println("Enter email");
+				sc.nextLine();
+				String email = sc.nextLine();
+				AddressBook obj = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
+				person.addContact(obj);
+			}
+		}
+		sc.close();
 
 	}
 
