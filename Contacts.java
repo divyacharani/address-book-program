3888f72ce67b9ac02607985892bbbb40ef358be0
unknown <divyacharaninabha@gmail.com>
2020-09-28 10:43:15 +0530

    Added: Method to edit a contact in address book is added

--- Contacts.java
+++ Contacts.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressBookProgram;
 
 import java.util.ArrayList;
 
+
 public class Contacts {
 
 	private ArrayList<AddressBook> contactList = new ArrayList<AddressBook>();
@@ -19,5 +20,26 @@ public class Contacts {
 	public void addContact(AddressBook obj) {
 		contactList.add(obj);
 		System.out.println("Contact Added Successfully");
+	}
+	
+	//Method to edit a contact
+	public void editContact(String firstName, String lastName, String address, String city, String state, int zip,
+			long phoneNumber, String email) {
+		for(AddressBook obj : contactList) {
+		if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+			obj.setFirstName(firstName);
+			obj.setLastName(lastName);
+			obj.setAddress(address);
+			obj.setCity(city);
+			obj.setState(state);
+			obj.setZip(zip);
+			obj.setPhoneNumber(phoneNumber);
+			obj.setEmail(email);
+			System.out.println("Contact Updated Successfully");
+			break;	
+		}
+		else
+			System.out.println("There is no contact with name "+firstName+" "+lastName);
+		}
 	}
 }
