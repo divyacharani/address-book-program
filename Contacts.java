0b38586bea67b47889d9ae5224fe627f509955a2
unknown <divyacharaninabha@gmail.com>
2020-09-29 11:50:44 +0530

    Added: Multiple contact objects were added

--- Contacts.java
+++ Contacts.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Contacts {
 
 	private ArrayList<AddressBook> contactList = new ArrayList<AddressBook>();
-	
+
 	//Setters and Getters
 	public ArrayList<AddressBook> getContactList() {
 		return contactList;
@@ -20,38 +20,34 @@ public class Contacts {
 		contactList.add(obj);
 		System.out.println("Contact Added Successfully");
 	}
-	
+
 	//Method to edit a contact
 	public void editContact(String firstName, String lastName, String address, String city, String state, int zip,
 			long phoneNumber, String email) {
 		for(AddressBook obj : contactList) {
-		if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
-			obj.setFirstName(firstName);
-			obj.setLastName(lastName);
-			obj.setAddress(address);
-			obj.setCity(city);
-			obj.setState(state);
-			obj.setZip(zip);
-			obj.setPhoneNumber(phoneNumber);
-			obj.setEmail(email);
-			System.out.println("Contact Updated Successfully");
-			break;	
-		}
-		else
-			System.out.println("There is no contact with name "+firstName+" "+lastName);
+			if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+				obj.setFirstName(firstName);
+				obj.setLastName(lastName);
+				obj.setAddress(address);
+				obj.setCity(city);
+				obj.setState(state);
+				obj.setZip(zip);
+				obj.setPhoneNumber(phoneNumber);
+				obj.setEmail(email);
+				System.out.println("Contact Updated Successfully");
+				break;	
+			}
 		}
 	}
-	
+
 	//Method to delete a contact
 	public void deleteContact(String firstName, String lastName) {
 		for(AddressBook obj : contactList) {
-		if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
-			contactList.remove(obj);
-			System.out.println("Contact Deleted Successfully");
-			break;	
-		}
-		else
-			System.out.println("There is no contact with name "+firstName+" "+lastName);
+			if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+				contactList.remove(obj);
+				System.out.println("Contact Deleted Successfully");
+				break;	
+			}
 		}
 	}
 }
