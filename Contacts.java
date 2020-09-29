4b59d12b8f9739b3ea9b90b50e684854e26e45d7
unknown <divyacharaninabha@gmail.com>
2020-09-29 11:58:05 +0530

    Refractored: Refractored to add multiple contact objects

--- Contacts.java
+++ Contacts.java
@@ -1,53 +1,54 @@
 package com.bridgelabz.addressBookProgram;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Contacts {
 
-	private ArrayList<AddressBook> contactList = new ArrayList<AddressBook>();
-
+	private Map<String,AddressBook> nameToContact = new HashMap<String,AddressBook>();
+	
 	//Setters and Getters
-	public ArrayList<AddressBook> getContactList() {
-		return contactList;
+	public Map<String, AddressBook> getNameToContact() {
+		return nameToContact;
 	}
 
-	public void setContactList(ArrayList<AddressBook> contactList) {
-		this.contactList = contactList;
+	public void setNameToContact(Map<String, AddressBook> nameToContact) {
+		this.nameToContact = nameToContact;
 	}
-
+	
 	//Method to add a contact
-	public void addContact(AddressBook obj) {
-		contactList.add(obj);
-		System.out.println("Contact Added Successfully");
+	public int addContact(String firstName, String lastName, AddressBook obj) {
+		nameToContact.put(firstName+" "+lastName,obj);
+		return 1;
 	}
-
+	
 	//Method to edit a contact
-	public void editContact(String firstName, String lastName, String address, String city, String state, int zip,
+	public int editContact(String firstName, String lastName, String address, String city, String state, int zip,
 			long phoneNumber, String email) {
-		for(AddressBook obj : contactList) {
-			if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
-				obj.setFirstName(firstName);
-				obj.setLastName(lastName);
-				obj.setAddress(address);
-				obj.setCity(city);
-				obj.setState(state);
-				obj.setZip(zip);
-				obj.setPhoneNumber(phoneNumber);
-				obj.setEmail(email);
-				System.out.println("Contact Updated Successfully");
-				break;	
+		for(String name : nameToContact.keySet()){
+			if(name.equals(firstName+" "+lastName)) {
+				nameToContact.get(name).setFirstName(firstName);
+				nameToContact.get(name).setLastName(lastName);
+				nameToContact.get(name).setAddress(address);
+				nameToContact.get(name).setCity(city);
+				nameToContact.get(name).setState(state);
+				nameToContact.get(name).setZip(zip);
+				nameToContact.get(name).setPhoneNumber(phoneNumber);
+				nameToContact.get(name).setEmail(email);
+				return 1;	
 			}
 		}
+		return -1;
 	}
-
+	
 	//Method to delete a contact
-	public void deleteContact(String firstName, String lastName) {
-		for(AddressBook obj : contactList) {
-			if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
-				contactList.remove(obj);
-				System.out.println("Contact Deleted Successfully");
-				break;	
+	public int deleteContact(String firstName, String lastName) {
+		for(String name : nameToContact.keySet()) {
+			if(name.equals(firstName+" "+lastName)) {
+				nameToContact.remove(firstName+" "+lastName);
+				return 1;
 			}
 		}
+		return -1;
 	}
+		
 }
